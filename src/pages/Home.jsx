3414b3183f57df434036b9e0a6a351c4d972267c
carlos8788 <carlos8788@gmail.com>
2023-08-20 23:07:43 -0300

    home styles ready

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,13 +4,38 @@ import '../assets/css/home.css'
 import { Control } from './components/Control'
 import { VuelosTabla } from './components/VuelosTabla'
 import MyComponent from './test/test'
+
+const HomeMain = () => {
+  return (
+    <section className='home-main'>
+      <p className='home-subtitle'>El tipo de alojamiento dice mucho sobre el viaje, ¿qué buscás para tus vacaciones o viaje?</p>
+      <div className="home-cards">
+        <div className="card">
+          <div className="imagen"></div>
+          <div className="card-title">Lorem</div>
+        </div>
+        <div className="card">
+          <div className="imagen"></div>
+          <div className="card-title">Lorem</div>
+        </div>
+        <div className="card">
+          <div className="imagen"></div>
+          <div className="card-title">Lorem</div>
+        </div>
+      </div>
+
+    </section>
+  )
+}
+
 export const Home = () => {
   return (
     <>
 
       <Control />
-      <VuelosTabla/>
-      
+      <HomeMain />
+      {/* <VuelosTabla/> */}
+
       {/* test */}
       {/* <MyComponent /> */}
     </>
